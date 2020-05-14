cf182c9664752237287b9dfa249615a77474fbe3
pluiel <pluiel@outlook.com>
2020-05-14 04:17:50 -0700

    Initial Commit

---
+++ Sound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sound
+//
+//  Created by Yuka Uematsu on 2020/05/14.
+//  Copyright © 2020 Uematsu Well. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
